f07f536b286a3d8f2bd76391d5c0755ceafb201a
Naufal-oboy <xorpro08@gamil.com>
2025-04-22 22:03:51 +0800

    posttest 5 selesai

---
+++ Kelas/pertemuan 5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+struct Address{
+    string kota;
+    string provinsi;
+    string negara;
+};
+
+
+
+
+int main () {
+    // Address address1;
+
+    // address1.kota = "samarinda";
+    // address1.provinsi = " kalimantan timur";
+    // address1.negara = "indonesia";
+
+    //  Address *address2 = &address1;
+
+
+    // cout<< address1.kota <<endl;
+
+    // cout<< address2.kota <<endl;
+
+    int a = 5;
+
+    int *b = &a;
+    return 0;
+}
